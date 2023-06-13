71bf841748e04b460ed90bfb5a8f1c8f4ecd9568
Ishmum <ishmum20000@gmail.com>
2023-06-14 03:36:19 +0800

    Add google and github signin options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    GithubAuthProvider,
+    signOut,
+} from "firebase/auth";
+import "./App.css";
+import initializeAuth from "./firebase/firebase.init";
+import { useState } from "react";
+
+initializeAuth();
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState({});
+
+    const handleGoogleSignin = () => {
+        const auth = getAuth();
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const { displayName, email, photoURL } = result.user;
+                const loggedInUser = {
+                    name: displayName,
+                    email: email,
+                    photo: photoURL,
+                };
+                setUser(loggedInUser);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential =
+                    GoogleAuthProvider.credentialFromError(error);
+            });
+    };
+
+    const handleGithubSignin = () => {
+        const auth = getAuth();
+
+        signInWithPopup(auth, githubProvider)
+            .then((result) => {
+                const { displayName, email, photoURL } = result.user;
+                const loggedInUser = {
+                    name: displayName,
+                    email: email,
+                    photo: photoURL,
+                };
+                setUser(loggedInUser);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential =
+                    GithubAuthProvider.credentialFromError(error);
+                // ...
+            });
+    };
+
+    const handleSignOut = () => {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+            setUser({});
+        });
+    };
+
+    return (
+        <div className="App">
+            {!user.name ? (
+                <div>
+                    <button onClick={handleGoogleSignin}>Google Sign in</button>
+                    <button onClick={handleGithubSignin}>Github Sign in</button>
+                </div>
+            ) : (
+                <button onClick={handleSignOut}>Sign out</button>
+            )}
+            {user.name && (
+                <div>
+                    <h2>Welcome {user.name}</h2>
+                    <div>Your email: {user.email}</div>
+                    <img src={user.photo} alt="" />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
