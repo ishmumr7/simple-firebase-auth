e5f07a65d23426a493fca62da727e05593a1b8d4
Ishmum <ishmum20000@gmail.com>
2023-06-13 18:07:37 +0800

    Add firebase and configuration

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPIyMT-mO9RXSPYomzhsj1LpKBdnOeHfU",
+    authDomain: "simple-firebase-auth-49d87.firebaseapp.com",
+    projectId: "simple-firebase-auth-49d87",
+    storageBucket: "simple-firebase-auth-49d87.appspot.com",
+    messagingSenderId: "1083684569340",
+    appId: "1:1083684569340:web:0b6ae4bdae6bfb98349779",
+};
+
+export default firebaseConfig
